8c986d04964217c69b5bc3c64a4e79d721d0db17
Kevin Malakoff <kmalakoff@gmail.com>
2024-02-24 06:44:27 -0800

    Upgrade dependencies

---
+++ dist/cjs/index.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+Object.defineProperty(exports, "default", {
+    enumerable: true,
+    get: function() {
+        return _default;
+    }
+});
+var _ora = /*#__PURE__*/ _interop_require_default(require("ora"));
+function _interop_require_default(obj) {
+    return obj && obj.__esModule ? obj : {
+        default: obj
+    };
+}
+var _default = function() {
+    var options = arguments.length > 0 && arguments[0] !== void 0 ? arguments[0] : {};
+    var message = options.message || "Building";
+    var spinner = (0, _ora.default)();
+    return {
+        name: "progress",
+        setup: function setup(build) {
+            build.onStart(function() {
+                spinner.text = "".concat(message, "\n");
+                spinner.start();
+            });
+            build.onEnd(function(result) {
+                result.errors.length ? spinner.fail("Build failed. ".concat(result.errors.length, " error").concat(result.errors.length > 1 ? "s" : "")) : spinner.succeed("Build successful");
+            });
+        }
+    };
+};
+/* CJS INTEROP */ if (exports.__esModule && exports.default) { Object.defineProperty(exports.default, '__esModule', { value: true }); for (var key in exports) exports.default[key] = exports[key]; module.exports = exports.default; }
