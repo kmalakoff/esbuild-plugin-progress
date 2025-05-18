adc56d76c71c5ff1dc1582ce6eb67d30695b97ce
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-17 19:47:41 -0700

    Updates

--- assets/navigation.js
+++ assets/navigation.js
@@ -1,1 +1,1 @@
-window.navigationData = "eJyLrlYqSa0oUbJSSklNSyzNKVHSUSpILMlQslJKK81LLsnMzyvWh0rpZZTk5ijpKGVn5qUoWZmZ1MYCAKPQFUI="
+window.navigationData = "eJyLrlYqSa0oUbJSSklNSyzNKVHSUSpILMlQslIqSyzKTEzKSS3Wh0rpZZTk5ijpKGVn5qUoWRkb1cYCAKAYFR0="
