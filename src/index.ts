7e9bf5b25e5cd2f285914ea5aa515654efe0b869
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-31 13:30:21 -0700

    Comprehensive types

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
+import type { Plugin, PluginBuild } from 'esbuild';
 import ora from 'ora';
 
 export interface Options {
   message?: string;
 }
 
-export default (options: Options = {}) => {
+export default (options: Options = {}): Plugin => {
   const message = options.message || 'Building';
   const spinner = ora();
 
   return {
     name: 'progress',
-    setup(build) {
+    setup(build: PluginBuild): undefined {
       build.onStart(() => {
         spinner.text = `${message}\n`;
         spinner.start();
